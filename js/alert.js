757768df79a90a205a5510fdd3dae7ffeac292a9
Yasaman <125014861+YasamanMirdoraghi@users.noreply.github.com>
2023-03-04 09:33:19 +0330

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,25 @@
+
+function show(){
+    let alert=document.getElementById("container")
+    alert.style.display="flex" 
+}
+function pen(){
+    let alert=document.getElementById("container")
+    alert.style.display="none" 
+}
+function showw(){
+    let alert=document.getElementById("containerr")
+    alert.style.display="flex" 
+}
+function penn(){
+    let alert=document.getElementById("containerr")
+    alert.style.display="none" 
+}
+function showww(){
+    let alert=document.getElementById("containerrr")
+    alert.style.display="flex" 
+}
+function pennn(){
+    let alert=document.getElementById("containerrr")
+    alert.style.display="none" 
+}
